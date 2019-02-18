6e839f6b7d76ba6ef8e098127cd4b94e7f74ada1
xiao-wen <47713595+xiao-wen-812@users.noreply.github.com>
2019-02-18 13:06:25 +0800

    Update waifu.js

--- live2d/js/waifu.js
+++ live2d/js/waifu.js
@@ -77,7 +77,7 @@ initTips();
         referrer.href = document.referrer;
     }
 
-    if(referrer.href !== '' && referrer.hostname != 'litblc.com'){
+    if(referrer.href !== '' && referrer.hostname != 'cnblogs.com/812-xiao-wen/'){
         var referrer = document.createElement('a');
         referrer.href = document.referrer;
         text = 'Hello! 来自 <span style="color:#0099cc;">' + referrer.hostname + '</span> 的朋友';
@@ -90,7 +90,7 @@ initTips();
             text = 'Hello! 来自 谷歌搜索 的朋友<br>欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
         }
     }else {
-        if (window.location.href == 'https://www.litblc.com/') { //如果是主页
+        if (window.location.href == 'https://www.cnblogs.com/812-xiao-wen/') { //如果是主页
             var now = (new Date()).getHours();
             if (now > 23 || now <= 5) {
                 text = '你是夜猫子呀？这么晚还不睡觉，明天起的来嘛';
